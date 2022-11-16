9f5cdd3c93d13523d5f660ee0def7723947670f3
elenajuslin <53784297+elenajuslin@users.noreply.github.com>
2022-11-16 18:16:27 +0200

    datalayer and css fixes

--- my-site-js.js
+++ my-site-js.js
@@ -1,5 +1,7 @@
-window.dataLayer = window.dataLayer || [];
-window.dataLayer.push({
- 'product': 'shirt',
- 'event': 'add_to_cart'
- });
+function dataLayer() {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+     'product': 'shirt',
+     'event': 'add_to_cart'
+});
+ }
