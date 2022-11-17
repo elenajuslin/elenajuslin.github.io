680f4c2c78f7250a5299df8d1b5c0678a141ba8a
elenajuslin <53784297+elenajuslin@users.noreply.github.com>
2022-11-17 13:08:57 +0200

    dl test4

--- my-site-js.js
+++ my-site-js.js
@@ -1,10 +1,10 @@
-function () {
+(function () {
     var button = document.getElementById("datalayer-test");
     button.addEventListener("click", function() {
         window.dataLayer.push({
             'product': 'shirt',
             'event': 'add_to_cart'
         });
-    }
+    });
 })();
     
