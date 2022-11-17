cda7f1a12e5d5832a6ecb8b7995a023aed81ce3d
elenajuslin <53784297+elenajuslin@users.noreply.github.com>
2022-11-17 13:14:49 +0200

    dl test5

--- my-site-js.js
+++ my-site-js.js
@@ -1,6 +1,7 @@
 (function datalayerTest() {
     var button = document.getElementById("datalayer-test");
     button.addEventListener("click", function() {
+        window.dataLayer = window.dataLayer || [];
         window.dataLayer.push({
             'product': 'shirt',
             'event': 'add_to_cart'
