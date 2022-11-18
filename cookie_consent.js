aab73265a3c236c2dd1d83aef90b11706e3a167c
elenajuslin <53784297+elenajuslin@users.noreply.github.com>
2022-11-18 16:12:14 +0200

    cookie consent stuff started

---
+++ cookie_consent.js
@@ -0,0 +1,9 @@
+//this is now just the gtm container
+if(document.getElementById('accept-cookies').clicked == true)
+{
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5GVN3LW');
+}
