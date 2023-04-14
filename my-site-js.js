584fffb348320280b31572359868f5195e914ff0
Elena Juslin <53784297+elenajuslin@users.noreply.github.com>
2023-04-14 14:46:19 +0300

    purchase DL added and ATC datalayer enriched

--- my-site-js.js
+++ my-site-js.js
@@ -5,6 +5,20 @@
         window.dataLayer.push({
             'product': 'shirt',
             'event': 'add_to_cart'
+            'ecommerce': {
+                'currencyCode': 'EUR',
+                'add': {                                // 'add' actionFieldObject measures.
+                  'products': [{                        //  adding a product to a shopping cart.
+                    'name': 'Triblend Android T-Shirt',
+                    'id': '12345',
+                    'price': '15.25',
+                    'brand': 'Google',
+                    'category': 'Apparel',
+                    'variant': 'Gray',
+                    'quantity': 1
+                   }]
+                }
+              }
         });
     });
 })();
